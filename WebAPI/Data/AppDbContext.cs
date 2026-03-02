7976b793d7888afae4f2509e2aea6d03b68a487b
Thiago Dutra <thiagoduutra01@gmail.com>
2026-03-02 08:44:10 -0300

    Feat: Realizada conexão com o banco

---
+++ WebAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+
+
+    }
+}
